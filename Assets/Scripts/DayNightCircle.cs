1a830afba06aab40bbcc6ef8b9fa567271a3f1f0
islamfazliyev <124509931+islamfazliyev@users.noreply.github.com>
2023-03-25 01:14:02 +0300

    Güncelleme notu3
    -artık 1 gündüz ve 1 gece olunca 1 gün gecti sayılıyor ve konsola x gün geçti yazdırılıyor

--- Assets/Scripts/DayNightCircle.cs
+++ Assets/Scripts/DayNightCircle.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class DayNightCircle : MonoBehaviour
 {
-    public float daynighttime;
     public float daynightspeed;
+    public float dayPassed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         // daynighttime = daynightspeed * Time.deltaTime;
-        transform.Rotate(daynighttime * Time.deltaTime, 0, 0);
+        transform.Rotate(daynightspeed * Time.deltaTime, 0, 0);
+        dayPassed += daynightspeed * Time.deltaTime;
+        if (dayPassed >= 360 && dayPassed <= 361)
+        {
+            Debug.Log("1 Gün Geçti");
+        }
+        else if (dayPassed >= 720 && dayPassed <= 721)
+        {
+            Debug.Log("2 Gün Geçti");
+        }
+        else if (dayPassed >= 1080 && dayPassed <= 1081)
+        {
+            Debug.Log("3 Gün Geçti");
+        }
     }
 }
